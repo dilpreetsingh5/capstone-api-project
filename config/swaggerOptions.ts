9d896b73f35e1cdaf9c78aa6682b71b3ea1bd3c7
dilpreetsingh5 <dilpreetsingh5@example.com>
2025-11-10 04:17:10 -0600

    feat(docs): integrate Swagger JSDoc for API documentation

---
+++ config/swaggerOptions.ts
@@ -0,0 +1,26 @@
+import swaggerJsdoc from "swagger-jsdoc";
+
+const swaggerOptions: swaggerJsdoc.Options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Financial Management API Documentation",
+            version: "1.0.0",
+            description:
+                "This is the API documentation for the Financial Management application, providing endpoints for managing accounts, transactions, and budgets.",
+        },
+        servers: [
+            {
+                url: "http://localhost:3000/api/v1",
+                description: "Local server",
+            },
+        ],
+    },
+    // Path to the API docs and schemas
+    apis: ["./src/api/v1/routes/*.ts", "./src/api/v1/validation/*.ts", "./src/api/v1/modules/*.ts"], // Path to the API docs and schemas
+};
+
+// Generate the Swagger spec
+export const generateSwaggerSpec = (): object => {
+    return swaggerJsdoc(swaggerOptions);
+};
